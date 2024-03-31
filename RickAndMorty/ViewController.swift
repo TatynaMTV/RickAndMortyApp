0a9a2edd85f274eb7086c89b5ec600544f43f4d1
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2024-03-31 13:59:01 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Татьяна Мальчик on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
